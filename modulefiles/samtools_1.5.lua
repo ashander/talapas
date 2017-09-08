200961980935209ccb8612eda315cc92fdcfc7c4
Jaime Ashander <ashander@ucdavis.edu>
2017-09-08 12:05:10 -0700

    Peter's samtools module

---
+++ modulefiles/samtools_1.5.lua
@@ -0,0 +1,8 @@
+whatis("samtools and bcftools provide tools for working with biological sequence data - www.htslib.org")
+whatis("Keyword: samtools bcftools vcftools htslib")
+prepend_path("PATH","/projects/apps/samtools_1.5/bin")
+prepend_path("CPATH","/projects/apps/samtools_1.5/include")
+prepend_path("LD_LIBRARY_PATH","/projects/apps/samtools_1.5/lib")
+prepend_path("LIBARY_PATH","/projects/apps/samtools_1.5/lib")
+prepend_path("MANPATH","/projects/apps/samtools_1.5/share/man")
+
