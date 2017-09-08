45d756199ffcaaceaca4d0d8f7e9936e6786ac28
Jaime Ashander <ashander@ucdavis.edu>
2017-09-08 14:05:45 -0700

    Fix path in msprime

--- modulefiles/msprime/0.4.1.dev.lua
+++ modulefiles/msprime/0.4.1.dev.lua
@@ -4,5 +4,5 @@ prereq("python3")
 prereq("bright/7.3")
 prereq("hdf5_18")
 prepend_path("PYTHONPATH","/projects/apps/popgen/installs/msprime_0.4.1.dev/lib/python3.6/site-packages/msprime-0.4.1.dev525+g569c994-py3.6-linux-x86_64.egg/")
-prepend_path("PATH","/projects/apps/popgen/installs/msprime-0.4.1.dev525+g569c994-py3.6-linux-x86_64.egg/bin")
+prepend_path("PATH","/projects/apps/popgen/installs/msprime_0.4.1.dev/bin")
 
