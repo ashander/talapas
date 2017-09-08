3fe671e419ac75e05e3bbfff3d473de94f91b9a3
Jaime Ashander <ashander@ucdavis.edu>
2017-09-08 12:04:58 -0700

    Fix path in ftprime/0.0.5

--- modulefiles/ftprime/0.0.5.lua
+++ modulefiles/ftprime/0.0.5.lua
@@ -4,6 +4,6 @@ prereq("msprime/0.4.1.dev")
 prereq("python3")
 prereq("bright/7.3")
 prereq("hdf5_18")
-prepend_path("PYTHONPATH","/projects/apps/popgen/installs/ftprime_0.0.5/lib/python3.6/site-packages/ftprime-0.0.5-py3.6-linux-x86_64.egg/")
+prepend_path("PYTHONPATH","/projects/apps/popgen/installs/ftprime_0.0.5/lib/python3.6/site-packages/ftprime-0.0.5-py3.6.egg/")
 prepend_path("PATH","/projects/apps/popgen/installs/ftprime_0.0.5/bin")
 
